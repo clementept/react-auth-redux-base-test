be342aae1215844380ca7a78b2a823c8370e54e3
Clemente <jopclem@gmail.com>
2024-03-07 16:10:44 +0000

    Removes token

--- src/features/auth/Welcome.jsx
+++ src/features/auth/Welcome.jsx
@@ -5,15 +5,12 @@ import { Link } from "react-router-dom";
 const Welcome = () => {
   const user = useSelector(selectCurrentUser);
   const token = useSelector(selectCurrentToken);
-  console.log(`token on Welcome: ${token}`)
 
   const welcome = user ? `Welcome ${user}!` : "Welcome!";
-  const tokenAbbr = `${token.slice(0, 9)}...`;
 
   const content = (
     <section className="welcome">
       <h1>{welcome}</h1>
-      <p>Token: {tokenAbbr}</p>
       <p>
         <Link to="/userslist">Go to the Users List</Link>
       </p>
