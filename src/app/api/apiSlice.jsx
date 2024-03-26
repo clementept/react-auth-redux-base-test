d7e179dea1c685a475bd044e7e72e06acaadfac5
Clemente <jopclem@gmail.com>
2024-03-26 22:35:40 +0000

    Fix retry on login and refresh

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../features/auth/authSlice";
 
-const BASE_URL = "https://api.clemdev.site/api"
+const BASE_URL = "http://localhost:8000/api"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
@@ -15,27 +15,30 @@ const baseQuery = fetchBaseQuery({
   },
 });
 
+const excludedEndpoints = ["/login", "/refresh"];
+
 const baseQueryWithReauth = async (args, api, extraOptions) => {
   let result = await baseQuery(args, api, extraOptions);
 
   if (result?.error?.status === 403) {
-    console.log("Sending refresh token");
+    // Check if the current endpoint is in the excluded list
+    if (!excludedEndpoints.includes(args.url)) {
+      console.log("Sending refresh token");
+      // send refresh token to get new access token
+      const refreshResult = await baseQuery("/login/refresh", api, extraOptions);
 
-    // send refresh token to get new access token
-    const refreshResult = await baseQuery("/login/refresh", api, extraOptions);
-    console.log(`Refresh result ${refreshResult}`);
+      if (refreshResult?.data) {
+        const user = api.getState().auth.user;
 
-    if (refreshResult?.data) {
-      const user = api.getState().auth.user;
+        //store new token
+        api.dispatch(setCredentials({ ...refreshResult.data, user }));
 
-      //store new token
-      api.dispatch(setCredentials({ ...refreshResult.data, user }));
-
-      // retry original query with new access token
-      result = await baseQuery(args, api, extraOptions);
-    } else {
-      console.log("Logging out");
-      api.dispatch(logOut());
+        // retry original query with new access token
+        result = await baseQuery(args, api, extraOptions);
+      } else {
+        console.log("Logging out");
+        api.dispatch(logOut());
+      }
     }
   }
   return result;
