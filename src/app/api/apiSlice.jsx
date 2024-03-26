54b050e712984d0bd2d011b221245a322a4f0d91
Clemente <jopclem@gmail.com>
2024-03-26 22:38:40 +0000

    Fix base url

--- src/app/api/apiSlice.jsx
+++ src/app/api/apiSlice.jsx
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials, logOut } from "../../features/auth/authSlice";
 
-const BASE_URL = "http://localhost:8000/api"
+const BASE_URL = "https://api.clemdev.site/api"
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
